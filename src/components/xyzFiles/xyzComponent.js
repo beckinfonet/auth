9777e76b7201e81afc8be9d8c30916520170fd5c
Beck TJ <bakytbek.tatibekov@gmail.com>
2018-04-06 23:38:28 -0400

    xyz component created

---
+++ src/components/xyzFiles/xyzComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class XYZ extends Coomponent {
+  render() {
+    return (
+      <div>
+        <p>XYZ should make API calls to Mongo</p>
+      </div>
+    );
+  }
+}
